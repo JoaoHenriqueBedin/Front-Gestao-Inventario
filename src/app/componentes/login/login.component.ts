242b4649606d721cbbf07b9994da4736cd9c624e
João Henrique Bedin Pedroso Dayko <hjh50029@gmail.com>
2024-10-09 16:31:47 -0300

    Novas Implementações do componente de login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { ImageModule } from 'primeng/image';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,ButtonModule,PasswordModule,InputTextModule,FloatLabelModule],
+  imports: [FormsModule,
+            ButtonModule,
+            PasswordModule,
+            InputTextModule,
+            FloatLabelModule,
+            ReactiveFormsModule,
+            CardModule,
+            ImageModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -16,6 +25,7 @@ export class LoginComponent {
 
   username!: string;
   password!: string;
+  form: any;
 
 
   register() {
