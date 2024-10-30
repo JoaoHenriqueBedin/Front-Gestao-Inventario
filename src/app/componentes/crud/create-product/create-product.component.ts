2723cb7d4402e8501038def47e174c2e6101d928
JoaoHenriqueBedin <jhbpdayko@minha.fag.edu.br>
2024-10-30 11:18:11 -0300

    fix: Criando Formulário Melhor de Produtos

--- src/app/componentes/crud/create-product/create-product.component.ts
+++ src/app/componentes/crud/create-product/create-product.component.ts
@@ -4,6 +4,13 @@ import { InputNumberModule } from 'primeng/inputnumber';
 import { FormsModule } from '@angular/forms';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { FileUploadEvent, FileUploadModule } from 'primeng/fileupload';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { MessagesModule } from 'primeng/messages';
+import { RippleModule } from 'primeng/ripple';
+import { Message } from 'primeng/api';
+
 @Component({
   selector: 'app-create-product',
   standalone: true,
@@ -12,16 +19,32 @@ import { FileUploadEvent, FileUploadModule } from 'primeng/fileupload';
     FormsModule,
     InputNumberModule,
     InputTextareaModule,
-    FileUploadModule
-
+    FileUploadModule,
+    FloatLabelModule,
+    CardModule,
+    ButtonModule,
+    MessagesModule,
+    RippleModule,
   ],
   templateUrl: './create-product.component.html',
-  styleUrl: './create-product.component.css'
+  styleUrl: './create-product.component.css',
 })
-export class CreateProductComponent {
-onUpload($event: FileUploadEvent) {
-throw new Error('Method not implemented.');
-}
-value: any;
 
+
+export class CreateProductComponent {
+
+  messages: Message[] | undefined;
+
+  addMessages() {
+    this.messages = [
+        { severity: 'success', summary: 'Produto Criado com Sucesso' },
+
+    ];
+}
+
+
+  onUpload($event: FileUploadEvent) {
+    throw new Error('Method not implemented.');
+  }
+  value: any;
 }
--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
